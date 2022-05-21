7149f6706d87b869a890c5fe0786f3c784f5100f
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2022-05-21 01:24:25 -0300

    feat(games): implament endpoint complete

---
+++ src/games/games.controller.ts
@@ -0,0 +1,53 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { CreateGamesDto } from "./dto/games-create.dto";
+import { UpdateGamesDto } from "./dto/games-update.dto";
+import { Games } from "./entities/game.entity";
+import { GamesService } from "./games.service";
+
+@ApiTags('Games')
+@Controller('Games')
+export class GamesController {
+  constructor(private readonly gamesService: GamesService) {}
+
+  @Get()
+  @ApiOperation({
+    summary: 'Lista todas os games',
+  })
+  findAll(): Promise<Games[]> {
+    return this.gamesService.findAll();
+  }
+  
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Lista um game',
+  })
+  findOne(@Param('id') id: string): Promise<Games> {
+    return this.gamesService.findOne(id);
+  }
+
+  @Post()
+  @ApiOperation({
+    summary: 'Criar uma game',
+  })
+  create(@Body() dto:CreateGamesDto ): Promise<Games> {
+    return this.gamesService.create(dto);
+  }
+
+  @Patch(':id')
+  @ApiOperation({
+    summary: 'Atualizar um game pelo id',
+  })
+  update(@Param('id') id: string, @Body() dto: UpdateGamesDto): Promise<Games> {
+    return this.gamesService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    summary: 'Remover um game pelo id',
+  })
+  delete(@Param('id') id: string) {
+    this.gamesService.delete(id);
+  }
+}

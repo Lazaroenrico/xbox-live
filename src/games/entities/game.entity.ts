c1756b94b64949be2e4a4c355f7605272072e04a
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2022-05-20 23:42:42 -0300

    feat(games): implament elemets for games

---
+++ src/games/entities/game.entity.ts
@@ -0,0 +1,7 @@
+export class Games{
+  id?:string;
+  name: string;
+  description: string;
+  price: number;
+  assessment:number;
+}
---
+++ src/games/games.module.ts
@@ -0,0 +1,6 @@
+import { Module } from "@nestjs/common";
+import { PrismaModule } from "src/prisma/prisma.module";
+
+@Module({
+  imports: [PrismaModule]
+})

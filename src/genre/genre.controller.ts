5e4e72c2cdcca17d6ad68ad0bf9daeed605d8bba
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2022-05-20 22:21:05 -0300

    feat(genre): implament endpoint complete

--- src/genre/genre.controller.ts
+++ src/genre/genre.controller.ts
@@ -1,30 +1,52 @@
-import { Controller, Delete, Get, Patch, Post } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-import { CreateGenreDto } from "./dto/create-genre.dto";
-import { GenreService } from "./genre.service";
-
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Genre } from '@prisma/client';
+import { CreateGenreDto } from './dto/create-genre.dto';
+import { UpdateGenreDto } from './dto/update-genre.dto';
+import { GenreService } from './genre.service';
 
 @ApiTags('Genre')
 @Controller('genre')
-export class GenreController{
-  constructor(private readonly genreService: GenreService){}
+export class GenreController {
+  constructor(private readonly genreService: GenreService) {}
 
   @Get()
-  findOne(){}
-
-  @Get()
-  findAll(){
-    return this.genreService.findAll()
+  @ApiOperation({
+    summary: 'Lista todas os generos',
+  })
+  findAll(): Promise<Genre[]> {
+    return this.genreService.findAll();
+  }
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Lista um genero',
+  })
+  findOne(@Param('id') id: string): Promise<Genre> {
+    return this.genreService.findOne(id);
   }
 
   @Post()
-  create(dto: CreateGenreDto){
-    return this.genreService.create()
+  @ApiOperation({
+    summary: 'Criar uma genero',
+  })
+  create(@Body() dto: CreateGenreDto): Promise<Genre> {
+    return this.genreService.create(dto);
   }
 
-  @Patch()
-  update(){}
+  @Patch(':id')
+  @ApiOperation({
+    summary: 'Atualizar um genero pelo id',
+  })
+  update(@Param('id') id: string, @Body() dto: UpdateGenreDto): Promise<Genre> {
+    return this.genreService.update(id, dto);
+  }
 
-  @Delete()
-  delete(){}
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    summary: 'Remover um genero pelo id',
+  })
+  delete(@Param('id') id: string) {
+    this.genreService.delete(id);
+  }
 }
--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -1,11 +1,62 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException, UnprocessableEntityException } from "@nestjs/common";
+import { Genre } from "@prisma/client";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateGenreDto } from "./dto/create-genre.dto";
+import { UpdateGenreDto } from "./dto/update-genre.dto";
 
 @Injectable()
 export class GenreService {
-  create() {
-    throw new Error("Method not implemented.");
+
+
+  constructor(private readonly prisma: PrismaService){}
+
+  async findById(id: string): Promise<Genre> {
+    const record = await this.prisma.genre.findUnique({ where: { id } });
+
+    if (!record) {
+      throw new NotFoundException(`Registro com ID '${id}' não encontrado.`);
+    }
+
+    return record;
   }
-  findAll() {
-    throw new Error("Method not implemented.");
+
+  async findOne(id: string): Promise<Genre> {
+    return this.findById(id);
+  }
+
+  findAll(): Promise<Genre[]> {
+    return this.prisma.genre.findMany();
+  }
+
+  async update(id: string, dto: UpdateGenreDto): Promise<Genre> {
+    await this.findById(id);
+    const data: Partial<Genre> = { ...dto };
+
+    return this.prisma.genre
+      .update({
+        where: { id },
+        data,
+      })
+      .catch(this.handleError);
+  }
+
+  create(dto: CreateGenreDto): Promise<Genre> {
+    const data = {genre: dto.genre}
+
+    return this.prisma.genre.create({ data }).catch(this.handleError);
+  }
+
+  async delete(id: string) {
+    await this.findById(id);
+
+    await this.prisma.genre.delete({ where: { id } });
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
+    throw new UnprocessableEntityException(
+      lastErrorLine || 'Alguem erro ocorreu ao executar a operação',
+    );
   }
 }

2a23e74c318100ba3d5280adc652b5b8be7c69af
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2022-05-21 02:02:04 -0300

    feat(games): implament endpoin complet

--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -1,6 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "src/prisma/prisma.module";
+import { GamesController } from "./games.controller";
+import { GamesService } from "./games.service";
 
 @Module({
-  imports: [PrismaModule]
+  imports: [PrismaModule],
+  controllers: [GamesController],
+  providers: [GamesService],
 })
+export class GamesModule{}
---
+++ src/games/games.service.ts
@@ -0,0 +1,63 @@
+import { Injectable, NotFoundException, UnprocessableEntityException } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateGamesDto } from "./dto/games-create.dto";
+import { UpdateGamesDto } from "./dto/games-update.dto";
+import { Games } from "./entities/game.entity";
+
+@Injectable()
+export class GamesService{
+  constructor(private readonly prisma: PrismaService){}
+
+
+  async findById(id: string): Promise<Games> {
+    const record = await this.prisma.games.findUnique({ where: { id } });
+
+    if (!record) {
+      throw new NotFoundException(`Registro com ID '${id}' não encontrado.`);
+    }
+
+    return record;
+  }
+
+  async findOne(id: string): Promise<Games> {
+    return this.findById(id);
+  }
+
+  findAll(): Promise<Games[]> {
+    return this.prisma.games.findMany();
+  }
+
+  async update(id: string, dto: UpdateGamesDto): Promise<Games> {
+    await this.findById(id);
+    const data: Partial<Games> = { ...dto };
+
+    return this.prisma.games
+      .update({
+        where: { id },
+        data,
+      })
+      .catch(this.handleError);
+  }
+
+  create(dto: CreateGamesDto): Promise<Games> {
+
+    const data: Games = {...dto}
+
+    return this.prisma.games.create({ data }).catch(this.handleError);
+  }
+
+  async delete(id: string) {
+    await this.findById(id);
+
+    await this.prisma.games.delete({ where: { id } });
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
+    throw new UnprocessableEntityException(
+      lastErrorLine || 'Alguem erro ocorreu ao executar a operação',
+    );
+  }
+
+}

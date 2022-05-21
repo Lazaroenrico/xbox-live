d8415e2d9bd4849f518db698121c1e606bee990b
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2022-05-21 01:41:52 -0300

    feat(app): implament endpoint complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GamesModule } from './games/games.module';
 import { GenreModule } from './genre/genre.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [GenreModule, PrismaModule],
+  imports: [GenreModule, PrismaModule, GamesModule],
   controllers: [AppController],
   providers: [AppService],
 })

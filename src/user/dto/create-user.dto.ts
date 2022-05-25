64b47c00999e7b9b21586695f05dab59af92f5d9
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2022-05-25 12:18:39 -0300

    feat(user): generate resource

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @ApiProperty({
+    example: 'Lázaro',
+  })
+  Name: string;
+
+  @IsString()
+  @ApiProperty({
+    example: 'georginho123@gmail.com',
+  })
+  Email: string;
+
+  @IsString()
+  @ApiProperty({
+    example: '12789123456',
+  })
+  CPF: string;
+
+  @IsBoolean()
+  @ApiProperty({
+    example: false,
+  })
+  Admin: boolean;
+}

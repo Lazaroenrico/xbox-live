2fa4fbafefff57b8e8d1341e43eb204966aa17d9
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2022-05-25 12:18:11 -0300

    feat(profile): generate resource

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,1 +1,17 @@
-export class CreateProfileDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class CreateProfileDto {
+  @IsString()
+  @ApiProperty({
+    example: 'George059',
+  })
+  Title: string;
+
+  @IsString()
+  @ApiProperty({
+    example:
+      'https://filestore.community.support.microsoft.com/api/profileimages/6b031d78-c348-4636-9e2a-09b5503ba0b8',
+  })
+  ImageUrl: string;
+}

0926bb0bb93ffe28d2539499ceaf542a61b3646f
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2022-05-25 15:56:17 -0300

    update 4

--- src/games/dto/games-create.dto.ts
+++ src/games/dto/games-create.dto.ts
@@ -11,22 +11,24 @@ export class CreateGamesDto {
 
   @IsString()
   @ApiProperty({
-    description: "Descrição do game",
+    description: 'Descrição do game',
     example:
       'Sea of Thieves oferece a experiência pirata definitiva, desde navegar e lutar até explorar e pilhar - tudo o que você precisa para viver a vida pirata e se tornar uma lenda por seus feitos. Sem nenhum papel definido, você tem total liberdade para interagir com o mundo e os outros jogadores como preferir.',
   })
   Description: string;
 
-  @IsNumber()
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
   @ApiProperty({
-    description:"Valor do game",
-    example: 199.90,
+    description: 'Valor do game',
+    example: 199.9,
   })
   Price: number;
 
   @IsUrl()
   @ApiProperty({
-    description:"Link da imagem do game",
+    description: 'Link da imagem do game',
     example:
       'https://store-images.s-microsoft.com/image/apps.16347.14554784103656548.6c0bfca6-ceff-4368-9bde-2fe50f344136.007dce43-6492-46f2-bb2b-2b28df98fc3c',
   })
@@ -34,7 +36,7 @@ export class CreateGamesDto {
 
   @IsNumber()
   @ApiProperty({
-    description:"Ano em que o game foi lançado",
+    description: 'Ano em que o game foi lançado',
     example: 2018,
   })
   Year: number;
@@ -43,21 +45,21 @@ export class CreateGamesDto {
   @Max(5)
   @IsNumber()
   @ApiProperty({
-    description:"Avaliação do game",
+    description: 'Avaliação do game',
     example: 5,
   })
   ImdbScore: number;
 
   @IsUrl()
   @ApiProperty({
-    description:"Link do trailer do game ( youtube )",
+    description: 'Link do trailer do game ( youtube )',
     example: 'https://www.youtube.com/watch?v=r5JIBaasuE8',
   })
   TrailerYoutubeUrl: string;
 
   @IsUrl()
   @ApiProperty({
-    description:"Gameplay do game",
+    description: 'Gameplay do game',
     example:
       'https://www.youtube.com/watch?v=TsuRGkeN22I&list=PLrVhyUnEQMV83h4gQ1CPIHut5swRfuU0B',
   })
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,6 +1,7 @@
 export class User {
   id?: string;
   Name: string;
+  Password: string;
   Email: string;
   CPF: string;
   Admin: boolean;
